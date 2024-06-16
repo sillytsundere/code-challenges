87744474ebabcebd9c3ffbce1d5638b0cad7ddeb
Paige <paigehcarroll@gmail.com>
2024-06-16 15:19:08 -0500

    created new challenge folder and added js function file

---
+++ opposites-attract/opposites-attract.js
@@ -0,0 +1,7 @@
+export function lovefunc(flower1, flower2) {
+  //   const loverOne = flower1 % 2;
+  //   const loverTwo = flower2 % 2;
+  //   return loverOne != loverTwo;
+
+  return flower1 % 2 != flower2 % 2;
+}
